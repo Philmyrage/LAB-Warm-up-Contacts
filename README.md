aa40cebae0ab7b012a56da2a4c611551cac5c197
PhillipWood93 <Phillipwood2023@outlook.com>
2024-12-05 12:13:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB-Warm-up-Contacts
+ Lab for module cs-210 module 7
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <format>
+
+/*
+A linked list is built in this lab. Make sure to keep track of the head node.
+
+(1) Define the class ContactNode per the following specifications:
+    Private data members
+    string contactName
+    string contactPhoneNumber
+    ContactNode* nextNodePtr
+    Constructor with parameters for name followed by phone number (1 pt)
+    Public member functions
+    GetName() - Accessor (1 pt)
+    GetPhoneNumber() - Accessor (1 pt)
+    InsertAfter() (2 pts)
+    GetNext() - Accessor (1 pt)
+    PrintContactNode()
+    Ex: If the name is Roxanne Hughes and the phone number is 443-555-2864, PrintContactNode() outputs:
+*/
+
+/*
+    (2) Define main() to read the name and phone number for three contacts and output each contact. Create three ContactNodes and use the nodes to build a linked list. (2 pts)
+*/
+
+/*
+    (3) Output the linked list using a loop to output contacts one at a time. (2 pts)
+*/
+class ContactNode
+{
+public:
+    /* Declare member functions here */
+
+private:
+    /* Declare data members here */
+};
+
+/* Define member functions here */
+
+int main()
+{
+    /* Type your code here. */
+
+    return 0;
+}
