04bae1007b4f50fb40d179b11b1cd33c247f4546
PhillipWood93 <Phillipwood2023@outlook.com>
2024-12-05 21:19:59 -0500

    Code passed all the tests.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,136 @@
 #include <iostream>
-#include <format>
+#include <string>
 
-/*
-A linked list is built in this lab. Make sure to keep track of the head node.
+// Only had to add this line for the test's in the lab
+using namespace std;
 
-(1) Define the class ContactNode per the following specifications:
-    Private data members
-    string contactName
-    string contactPhoneNumber
-    ContactNode* nextNodePtr
-    Constructor with parameters for name followed by phone number (1 pt)
-    Public member functions
-    GetName() - Accessor (1 pt)
-    GetPhoneNumber() - Accessor (1 pt)
-    InsertAfter() (2 pts)
-    GetNext() - Accessor (1 pt)
-    PrintContactNode()
-    Ex: If the name is Roxanne Hughes and the phone number is 443-555-2864, PrintContactNode() outputs:
-*/
-
-/*
-    (2) Define main() to read the name and phone number for three contacts and output each contact. Create three ContactNodes and use the nodes to build a linked list. (2 pts)
-*/
-
-/*
-    (3) Output the linked list using a loop to output contacts one at a time. (2 pts)
-*/
 class ContactNode
 {
 public:
-    /* Declare member functions here */
+    // Constructors
+    ContactNode()
+    {
+        contactName = "";
+        contactPhoneNumber = "";
+        nextNodePtr = nullptr;
+    }
+    ContactNode(std::string name, std::string phoneNumber)
+    {
+        contactName = name;
+        contactPhoneNumber = phoneNumber;
+        nextNodePtr = nullptr;
+    }
+    // The Big Three implementations...
+    ContactNode(const ContactNode &other)
+    {
+        contactName = other.contactName;
+        contactPhoneNumber = other.contactPhoneNumber;
+        nextNodePtr = new ContactNode;
+        *nextNodePtr = *other.nextNodePtr;
+    }
+
+    ~ContactNode()
+    {
+        delete nextNodePtr;
+    }
+
+    ContactNode &operator=(const ContactNode &other)
+    {
+        // Exit early if the object is being assigned to itself
+        if (this == &other)
+            return *this;
+
+        // Deallocate the nextNodePtr
+        delete nextNodePtr;
+        // Allocate a new nextNodePtr
+        nextNodePtr = new ContactNode;
+        // copy the data held at the nextNodePtr address
+        *nextNodePtr = *other.nextNodePtr;
+        return *this;
+    }
+
+    // Accessors
+    std::string GetName() const { return contactName; }
+    std::string GetPhoneNumber() const { return contactPhoneNumber; }
+    ContactNode *GetNext() const { return nextNodePtr; }
+
+    // Mutators
+    void SetName(std::string name) { contactName = name; }
+    void SetPhoneNumber(std::string phoneNumber) { contactPhoneNumber = phoneNumber; }
+
+    // Insertion
+    void InsertAfter(ContactNode *Location)
+    {
+        ContactNode *temp = nullptr;
+        temp = this->nextNodePtr;
+        this->nextNodePtr = Location;
+        Location->nextNodePtr = temp;
+    }
+
+    // output
+    void PrintContactNode() const
+    {
+        std::cout << "Name: " << contactName << std::endl;
+        std::cout << "Phone number: " << contactPhoneNumber << std::endl;
+    }
 
 private:
     /* Declare data members here */
+    std::string contactName;
+    std::string contactPhoneNumber;
+    ContactNode *nextNodePtr;
 };
 
 /* Define member functions here */
 
 int main()
 {
-    /* Type your code here. */
+    ContactNode *head = new ContactNode();
+    ContactNode *tail = nullptr;
+    ContactNode *current = nullptr;
+    std::string tmp = "";
+
+    // Initialize the head with user input...
+    getline(std::cin, tmp);
+    head->SetName(tmp);
+
+    getline(std::cin, tmp);
+    head->SetPhoneNumber(tmp);
+
+    tail = head;
+
+    // Get the user input and add it to the list
+    for (int i = 0; i < 2; i++)
+    {
+        std::string tname = "";
+        std::string tphone = "";
+        getline(std::cin, tname);
+        getline(std::cin, tphone);
+
+        ContactNode *current = new ContactNode(tname, tphone);
+        tail->InsertAfter(current);
+        tail = current;
+    }
+
+    current = head;
+    int count = 1;
+    while (current != nullptr)
+    {
+        std::cout << "Person " << count << ": " << current->GetName() << ", " << current->GetPhoneNumber() << std::endl;
+        current = current->GetNext();
+        count++;
+    }
+
+    current = head;
+    // print the contents of the list
+    std::cout << "\nCONTACT LIST"
+              << std::endl;
+    while (current != nullptr)
+    {
+        current->PrintContactNode();
+        current = current->GetNext();
+        std::cout << std::endl;
+    }
 
     return 0;
 }
